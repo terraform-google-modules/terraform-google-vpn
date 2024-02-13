ed6eacf80f6153384c7a8bb6671c8b69aa04f9fa
Awais Malik <awmalik@google.com>
2024-02-12 16:19:46 -0800

    fix: removing 1.3 version constraint for GCP Cloud Setup

--- modules/vpn_ha/versions.tf
+++ modules/vpn_ha/versions.tf
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = ">=0.13.0, < 1.3"
+  required_version = ">=0.13.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -32,9 +32,9 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-vpn/v2.3.2"
+    module_name = "blueprints/terraform/terraform-google-vpn/v2.3.3"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/terraform-google-vpn/v2.3.2"
+    module_name = "blueprints/terraform/terraform-google-vpn/v2.3.3"
   }
 }
