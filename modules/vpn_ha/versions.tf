b06143d3c83ec57afd3512d3ed1be0db2a6a0520
Ilia <ilia.lazebnik@placer.ai>
2025-02-27 12:37:34 -0500

    bump min version

--- modules/vpn_ha/versions.tf
+++ modules/vpn_ha/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.7, < 7"
+      version = ">= 6.21, < 7"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.7, < 7"
+      version = ">= 6.21, < 7"
     }
     random = {
       source  = "hashicorp/random"
